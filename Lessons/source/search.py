cf549ee07a5da2a4a07c6aa11d6bb1178651ee7f
Ikey Benzaken <ikey.benz@gmail.com>
2019-04-04 12:52:10 -0700

    Added binary search methods

--- Lessons/source/search.py
+++ Lessons/source/search.py
@@ -1,45 +1,82 @@
 #!python
 
 def linear_search(array, item):
-    """return the first index of item in array or None if item is not found"""
-    # implement linear_search_iterative and linear_search_recursive below, then
-    # change this to call your implementation to verify it passes all tests
-    return linear_search_iterative(array, item)
-    # return linear_search_recursive(array, item)
+  """return the first index of item in array or None if item is not found"""
+  # implement linear_search_iterative and linear_search_recursive below, then
+  # change this to call your implementation to verify it passes all tests
+  # return linear_search_iterative(array, item)
+  return linear_search_recursive(array, item)
 
 
 def linear_search_iterative(array, item):
-    # loop over all array values until item is found
-    for index, value in enumerate(array):
-        if item == value:
-            return index  # found
-    return None  # not found
+  # loop over all array values until item is found
+  for index, value in enumerate(array):
+    if item == value:
+      return index  # found
+  return None  # not found
 
 
 def linear_search_recursive(array, item, index=0):
-    # TODO: implement linear search recursively here
-    pass
-    # once implemented, change linear_search to call linear_search_recursive
-    # to verify that your recursive implementation passes all tests
+  
+  if index >= len(array):
+    return None
+  if array[index] == item:
+    return index
+
+  return linear_search_recursive(array, item, index+1)
 
 
 def binary_search(array, item):
-    """return the index of item in sorted array or None if item is not found"""
-    # implement binary_search_iterative and binary_search_recursive below, then
-    # change this to call your implementation to verify it passes all tests
-    return binary_search_iterative(array, item)
-    # return binary_search_recursive(array, item)
+  """return the index of item in sorted array or None if item is not found"""
+  # implement binary_search_iterative and binary_search_recursive below, then
+  # change this to call your implementation to verify it passes all tests
+  # return binary_search_iterative(array, item)
+  return binary_search_recursive(array, item)
 
 
 def binary_search_iterative(array, item):
-    # TODO: implement binary search iteratively here
-    pass
-    # once implemented, change binary_search to call binary_search_iterative
-    # to verify that your iterative implementation passes all tests
+  start, stop = 0, len(array) - 1
+
+  midpoint = (start + stop) // 2
+
+  while array[midpoint] is not item and start < stop:
+    if array[midpoint] < item:
+      start = midpoint + 1
+    else:
+      stop = midpoint
+    midpoint = (start + stop) // 2
+
+  if array[midpoint] is item:
+    return midpoint
+  
+  return None
 
 
 def binary_search_recursive(array, item, left=None, right=None):
-    # TODO: implement binary search recursively here
-    pass
-    # once implemented, change binary_search to call binary_search_recursive
-    # to verify that your recursive implementation passes all tests
+  if not left: left = 0
+  if not right: right = len(array) - 1
+
+  midpoint = (right + left) // 2
+
+  if array[midpoint] == item:
+    return midpoint
+  elif left == right:
+    return None
+  elif array[midpoint] > item:
+    return binary_search_recursive(array, item, left, midpoint)
+  return binary_search_recursive(array, item, midpoint+1, right) # +1 because we floor the midpoint
+  
+
+# EXAMPLE:
+#   array = [1,2,3,4,5,6], item = 2
+#   
+#   left | right | midpoint | array[midpoint]
+#     0  |   5   |    2     |       3        
+#     0  |   2   |    1     |       2    == item -> return midpoint (1)
+# 
+#   array = [1,2,3,4,5,6], item = 6
+#   left | right | midpoint | array[midpoint]
+#     0  |   5   |    2     |       3
+#     2  |   5   |    3     |       4
+#     3  |   5   |    4     |       5
+#     4  |   5   |    4
