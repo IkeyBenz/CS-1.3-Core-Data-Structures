49ec37c15def3242584df0108a438c4c6f53691d
Ikey Benzaken <ikey.benz@gmail.com>
2019-04-03 17:55:36 -0700

    Added recursion methods

--- Lessons/source/recursion.py
+++ Lessons/source/recursion.py
@@ -9,14 +9,19 @@ def factorial(n):
     # implement factorial_iterative and factorial_recursive below, then
     # change this to call your implementation to verify it passes all tests
     # return factorial_iterative(n)
-    return factorial_recursive(n)
+    return factorial_iterative(n)
 
 
 def factorial_iterative(n):
     # TODO: implement the factorial function iteratively here
-    pass
-    # once implemented, change factorial (above) to call factorial_iterative
-    # to verify that your iterative implementation passes all tests
+    
+    result = 1 # Initialize result to be equal to 1
+
+    while n: # While n is not zero
+      result *= n
+      n -= 1
+
+    return result
 
 
 def factorial_recursive(n):
